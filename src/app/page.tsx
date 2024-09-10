31e1dea4612286aeddb9b7117607b5d8c2796b85
kiejibi <kaekie2004@gmail.com>
2024-09-11 00:24:26 +0700

    ADD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ import {
   Title,
 } from "@mantine/core";
 
-import { Form, useForm, zodResolver } from "@mantine/form";
+import { useForm, zodResolver } from "@mantine/form";
 import { z } from "zod";
 
 const schema = z
@@ -35,20 +35,23 @@ const schema = z
       .min(3, { message: "Last name must have at least 3 characters" }),
     email: z.string().email({ message: "Invalid email format" }),
     plan: z.enum(["funrun", "mini", "half", "full"], {
-      errorMap: (issue, ctx) => ({ message: "Please select a plan" }),
+      errorMap: () => ({ message: "Please select a plan" }),
     }),
     gender: z.enum(["male", "female"], {
-      errorMap: (issue, ctx) => ({ message: "Please choose a gender" }),
+      errorMap: () => ({ message: "Please choose a gender" }),
     }),
     acceptTermsAndConds: z.literal(true, {
       // message: "You must accept terms and conditions",
-      errorMap: (issue, ctx) => ({
+      errorMap: () => ({
         message: "You must accept terms and conditions",
       }),
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
+    password: z
+      .string()
+      .min(6, { message: "Password must contain at least 6 characters" })
+      .max(12, { message: "Password must not exceed 12 characters" }),
     confirmPassword: z.string(),
   })
   .refine(
@@ -69,7 +72,16 @@ const schema = z
       message: "Invalid coupon code",
       path: ["coupon"],
     }
-  );
+  )
+  .refine(
+    (data) => {
+      if (data.confirmPassword === data.password) return true;
+    },
+    {
+      message: "Password does not match",
+      path: ["confirmPassword"]
+    }
+  )
 
 export default function Home() {
   const [opened, { open, close }] = useDisclosure(false);
@@ -98,8 +110,11 @@ export default function Home() {
     if (form.values.plan === "funrun") price = 500;
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
-
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "half") price = 1200;
+    if (form.values.plan === "full") price = 1500;
     //check discount here
+    if (form.values.hasCoupon && form.values.coupon === "CMU2023") price = price - (price * (30 / 100));
 
     return price;
   };
@@ -114,7 +129,7 @@ export default function Home() {
         <Space h="lg" />
 
         {/* add form */}
-        <form onSubmit={form.onSubmit((v) => alert("See you at CMU Marathon"))}>
+        <form onSubmit={form.onSubmit(() => alert("See you at CMU Marathon 2024"))}>
           <Stack gap="sm">
             <Group grow align="start">
               <TextInput
@@ -167,7 +182,7 @@ export default function Home() {
 
             <Text fw="bold">
               {" "}
-              Total Price : {computePrice().toLocaleString()} BATH
+              Total Price : {computePrice().toLocaleString()} BAHT
             </Text>
 
             <Divider variant="dashed" />
@@ -187,7 +202,7 @@ export default function Home() {
           </Stack>
         </form>
 
-        {/* <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" /> */}
+        <Footer year={2024} fullName="Hathairat Promchauy" studentId={660610804} />
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
